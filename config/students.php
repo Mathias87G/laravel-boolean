f7807c13dd0083b0b2a8dcceccfa6ade7244d150
Mathias <mathias.grandin@gmail.com>
2020-10-13 16:17:15 +0200

    slug

--- config/students.php
+++ config/students.php
@@ -5,6 +5,7 @@ return [
     'img' => 'https://www.boolean.careers/images/people/ortenzi.png',
     'nome' => 'Daniela Ortenzi',
     'anni' => 27,
+    'slug' => 'dany',
     'azienda' => 'RDS Radio Dimensione Suono',
     'ruolo' => 'Web Developer',
     'descrizione' => 'Laureata in psicologia alla Sapienza di Roma con una nascente carriere nella SEO, Daniela si iscrive al corso Boolean. Ora lavora come web developer presso RDS Radio Dimensione Suono.'
@@ -13,6 +14,7 @@ return [
     'img' => 'https://www.boolean.careers/images/people/antidormi.png',
     'nome' => 'Nicholas Antidormi',
     'anni' => 20,
+    'slug' => 'niky',
     'azienda' => 'Atoms Studio',
     'ruolo' => 'Web Developer',
     'descrizione' => 'Intraprende gli studi universitari in matematica per le sue doti analitiche, ma abbandona il percorso per concentrarsi su uno più pratico e orientato alla richiesta del mercato del lavoro: la programmazione.'
@@ -21,6 +23,7 @@ return [
     'img' => 'https://www.boolean.careers/images/people/biagini.png',
     'nome' => 'Alessandro Biagini',
     'anni' => 26,
+    'slug' => 'ale',
     'azienda' => 'DISC SPA',
     'ruolo' => 'Web Developer',
     'descrizione' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.'
@@ -29,6 +32,7 @@ return [
     'img' => 'https://www.boolean.careers/images/people/quarticelli.png',
     'nome' => 'Simone Quarticelli',
     'anni' => 32,
+    'slug' => 'simo',
     'azienda' => 'VLC 2',
     'ruolo' => 'Web Developer',
     'descrizione' => 'Per supportare la famiglia, Simone inizia a lavorare come operaio da giovanissimo. Fortemente motivato a cambiare vita, per 6 mesi non stacca mai lo sguardo dal traguardo, distinguendosi per tenacia e resilienza.'
@@ -37,6 +41,7 @@ return [
     'img' => 'https://www.boolean.careers/images/people/giannone.jpeg',
     'nome' => 'Valentina Giannone',
     'anni' => 38,
+    'slug' => 'vale',
     'azienda' => 'Bloovery',
     'ruolo' => 'Web Developer',
     'descrizione' => 'Un passato come responsabile di laboratorio odontotecnico, per poi decidere che non era la sua vera vocazione. Valentina ha deciso di prendere il coraggio a due mani e lanciarsi con successo nel mondo del web development.'
@@ -44,6 +49,7 @@ return [
   [
     'img' => 'https://www.boolean.careers/images/people/davide.png',
     'nome' => 'Davide Patruno',
+    'slug' => 'patro',
     'anni' => 29,
     'azienda' => 'ArmadioVerde',
     'ruolo' => 'Web Developer',
--- resources/views/slug.blade.php
+++ resources/views/slug.blade.php
@@ -4,17 +4,17 @@
 @section('main')
 <div class="container">
   <div class="card-group d-flex">
-    @foreach ($data as $key => $student)
+
     <div class="card">
       <img src="{{$student['img']}}" class="card-img-top" alt="{{$student['nome']}}">
       <div class="card-body">
-        <a href="{{ route('Student.show', ['id' => $key]) }}"><h5 class="card-title">{{$student['nome']}}, {{$student['anni']}}</h5></a>
+        <h5 class="card-title">{{$student['nome']}}, {{$student['anni']}}</h5>
         <h6 class="card-title">{{$student['ruolo']}}</h6>
-        {{-- <p class="card-text">{{$student['descrizione']}}</p> --}}
+        <p class="card-text">{{$student['descrizione']}}</p>
         <p class="card-text"><small class="text-muted">Assunto da: {{$student['azienda']}}</small></p>
       </div>
     </div>
-    @endforeach
+
   </div>
 </div>
 @endsection
